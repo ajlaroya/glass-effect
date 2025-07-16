c9b3a40810e0da09a2c2d18564b4018bde08f49f
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-07-16 17:05:16 +1000

    transmission material, leva controls

--- components/Scene/Model.jsx
+++ components/Scene/Model.jsx
@@ -1,28 +1,40 @@
-import { useGLTF, Text, MeshTransmissionMaterial } from "@react-three/drei"
-import { useFrame, useThree } from "@react-three/fiber"
-import { useRef } from "react"
+import { useGLTF, Text, MeshTransmissionMaterial } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
+import { useRef } from "react";
 
 const Model = () => {
   const mesh = useRef();
-  const { nodes } = useGLTF('/medias/torrus.glb')
-  const { viewport } = useThree()
+  const { nodes } = useGLTF("/medias/torrus.glb");
+  const { viewport } = useThree();
 
   useFrame(() => {
-    mesh.current.rotation.x += 0.002
-  })
+    mesh.current.rotation.x += 0.002;
+  });
 
-  // 11:47
+  const materialProps = useControls({
+    thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
+    roughness: { value: 0, min: 0, max: 1, step: 0.1 },
+    transmission: { value: 1, min: 0, max: 1, step: 0.1 },
+    ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
+    chromaticAberration: { value: 0.02, min: 0, max: 1 },
+    backside: { value: true },
+  });
 
   return (
     <group scale={viewport.width / 3.5}>
-      <Text fontSize={0.5} font="fonts/PPNeueMontreal-Bold.otf" position={[0, 0, -0.5]}>
-        hello world!
+      <Text
+        fontSize={0.5}
+        font="fonts/PPNeueMontreal-Bold.otf"
+        position={[0, 0, -0.5]}
+      >
+        QKC Studios
       </Text>
       <mesh ref={mesh} {...nodes.Torus002}>
-        <MeshTransmissionMaterial />
+        <MeshTransmissionMaterial {...materialProps} />
       </mesh>
     </group>
-  )
-}
+  );
+};
 
-export default Model
+export default Model;
